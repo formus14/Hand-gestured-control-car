cdde68849b9b0fa50f5dacf0af69e7ff226cfa05
formus14 <formus14@users.noreply.github.com>
2015-05-18 01:31:20 +0200

    Create Glove_Code.ino
    upload this code to the micro controller  attached to the glove

---
+++ Glove_Code.ino
@@ -0,0 +1,418 @@
+/* 
+ Connect the Acceloremeter to your microcontroller as shown below:
+ MMA8452 Breakout ------------ Arduino
+ 3.3V --------------------- 3.3V
+ SDA -------^^(1k)^^------- A4
+ SCL -------^^(1k)^^------- A5
+ GND ---------------------- GND
+ */
+
+#include <Wire.h> // Used for I2C
+
+// The SparkFun breakout board defaults to 1, set to 0 if SA0 jumper on the bottom of the board is set
+#define MMA8452_ADDRESS 0x1D  // 0x1D if SA0 is high, 0x1C if low
+
+//Define a few of the registers that we will be accessing on the MMA8452
+#define OUT_X_MSB 0x01
+#define XYZ_DATA_CFG  0x0E
+#define WHO_AM_I   0x0D
+#define CTRL_REG1  0x2A
+
+#define GSCALE 2 // Sets full-scale range to +/-2, 4, or 8g. Used to calc real g values.
+
+
+#define PIN_SCE   7
+#define PIN_RESET 6
+#define PIN_DC    5
+#define PIN_SDIN  11
+#define PIN_SCLK  13
+
+#define LCD_C     LOW
+#define LCD_D     HIGH
+
+#define LCD_X     84
+#define LCD_Y     48
+#define LCD_COMMAND  0 
+
+
+int flexSensorPin1 = A0; // 1st Flex sensor is connected to analog pin 0
+int flexSensorPin2 = A1; // 2nd Flex sensor is connected to analog pin 1
+int flag=true;
+int potvalue = 0;
+
+
+void setup()
+{
+  Serial.begin(9600);
+  Serial.println("MMA8452");
+  Wire.begin(); //Join the bus as a master
+  initMMA8452(); //Test and intialize the MMA8452
+  LcdInitialise();
+  LcdClear();
+  setContrast(60); // Pretty good value, play around with it
+  
+}
+
+void loop()
+{  
+     if (flag==true)
+      {
+       Serial.println('b');
+       flag=false;
+      }
+  calc();
+  
+     int flexSensorReading1 = analogRead(flexSensorPin1); 
+     int flexSensorReading2 = analogRead(flexSensorPin2);
+ 
+   if ( flexSensorReading1 < 999)
+     {
+       Serial.println("1");   
+    gotoXY(18,12);
+    LcdString("        ");
+    gotoXY(18,0);
+    LcdString("forward 1");
+     
+     }  
+    else if ( flexSensorReading1 < 1002)
+     {
+    Serial.println('2'); 
+    gotoXY(18,12);
+    LcdString("        ");
+    gotoXY(18,0);
+    LcdString("forward 2");
+      
+     }  
+   else   if (( flexSensorReading1 > 1002) && (flexSensorReading2 < 1000)) 
+     {
+    Serial.println('3');
+    gotoXY(18,12);
+    LcdString("        ");
+    gotoXY(18,0);
+    LcdString("forward 3");
+    
+     }  
+    else if ((flexSensorReading2 > 1000) &&  (flexSensorReading1 > 1004))
+     {
+    Serial.println('6');
+    gotoXY(18,12);
+    LcdString("backward");
+    gotoXY(18,0);
+    LcdString("         ");
+    
+     }
+     
+}
+void calc()
+{
+  int accelCount[3];  // Stores the 12-bit signed value
+  readAccelData(accelCount);  // Read the x/y/z adc values
+
+  // Now we'll calculate the accleration value into actual g's
+  float accelG[3];  // Stores the real accel value in g's
+  for (int i = 0 ; i < 3 ; i++)
+  {
+    accelG[i] = (float) accelCount[i] / ((1<<12)/(2*GSCALE));  // get actual g value, this depends on scale being set
+  }
+
+    if (accelG[1] < -0.7)
+    {
+     Serial.println('5');
+    
+    gotoXY(50,10);
+    LcdString("     ");
+    gotoXY(0,10);
+    LcdString("left");
+  //   gotoXY(16,10);
+   // LcdString(" ");
+    }
+else
+  if (accelG[0] < 0.3)
+  {
+     Serial.println('4');
+     gotoXY(50,10);
+     LcdString("right     ");
+    gotoXY(0,10);
+    LcdString("   ");
+    
+    gotoXY(29,10);
+    LcdString(" ");
+  }
+  else
+  {
+    gotoXY(50,10);
+     LcdString("      ");
+    gotoXY(0,10);
+    LcdString("     ");
+    gotoXY(29,10);
+    LcdString("    ");
+  }
+
+
+
+} 
+
+void readAccelData(int *destination)
+{
+  byte rawData[6];  // x/y/z accel register data stored here
+
+  readRegisters(OUT_X_MSB, 6, rawData);  // Read the six raw data registers into data array
+
+  // Loop to calculate 12-bit ADC and g value for each axis
+  for(int i = 0; i < 3 ; i++)
+  {
+    int gCount = (rawData[i*2] << 8) | rawData[(i*2)+1];  //Combine the two 8 bit registers into one 12-bit number
+    gCount >>= 4; //The registers are left align, here we right align the 12-bit integer
+
+    // If the number is negative, we have to make it so manually (no 12-bit data type)
+    if (rawData[i*2] > 0x7F)
+    {  
+      gCount = ~gCount + 1;
+      gCount *= -1;  // Transform into negative 2's complement #
+    }
+
+    destination[i] = gCount; //Record this gCount into the 3 int array
+  }
+}
+
+
+void initMMA8452()
+{
+  byte c = readRegister(WHO_AM_I);  // Read WHO_AM_I register
+  if (c == 0x2A) // WHO_AM_I should always be 0x2A
+  {  
+    Serial.println("MMA8452Q is online...");
+  }
+  else
+  {
+    Serial.print("Could not connect to MMA8452Q: 0x");
+    Serial.println(c, HEX);
+    while(1) ; // Loop forever if communication doesn't happen
+  }
+
+  MMA8452Standby();  // Must be in standby to change registers
+
+  // Set up the full scale range to 2, 4, or 8g.
+  byte fsr = GSCALE;
+  if(fsr > 8) fsr = 8; //Easy error check
+  fsr >>= 2; // Neat trick, see page 22. 00 = 2G, 01 = 4A, 10 = 8G
+  writeRegister(XYZ_DATA_CFG, fsr);
+
+  //The default data rate is 800Hz and we don't modify it in this example code
+
+  MMA8452Active();  // Set to active to start reading
+}
+
+// Sets the MMA8452 to standby mode. It must be in standby to change most register settings
+void MMA8452Standby()
+{
+  byte c = readRegister(CTRL_REG1);
+  writeRegister(CTRL_REG1, c & ~(0x01)); //Clear the active bit to go into standby
+}
+
+// Sets the MMA8452 to active mode. Needs to be in this mode to output data
+void MMA8452Active()
+{
+  byte c = readRegister(CTRL_REG1);
+  writeRegister(CTRL_REG1, c | 0x01); //Set the active bit to begin detection
+}
+
+// Read bytesToRead sequentially, starting at addressToRead into the dest byte array
+void readRegisters(byte addressToRead, int bytesToRead, byte * dest)
+{
+  Wire.beginTransmission(MMA8452_ADDRESS);
+  Wire.write(addressToRead);
+  Wire.endTransmission(false); //endTransmission but keep the connection active
+
+  Wire.requestFrom(MMA8452_ADDRESS, bytesToRead); //Ask for bytes, once done, bus is released by default
+
+  while(Wire.available() < bytesToRead); //Hang out until we get the # of bytes we expect
+
+  for(int x = 0 ; x < bytesToRead ; x++)
+    dest[x] = Wire.read();    
+}
+
+// Read a single byte from addressToRead and return it as a byte
+byte readRegister(byte addressToRead)
+{
+  Wire.beginTransmission(MMA8452_ADDRESS);
+  Wire.write(addressToRead);
+  Wire.endTransmission(false); //endTransmission but keep the connection active
+
+  Wire.requestFrom(MMA8452_ADDRESS, 1); //Ask for 1 byte, once done, bus is released by default
+
+  while(!Wire.available()) ; //Wait for the data to come back
+  return Wire.read(); //Return this one byte
+}
+
+// Writes a single byte (dataToWrite) into addressToWrite
+void writeRegister(byte addressToWrite, byte dataToWrite)
+{
+  Wire.beginTransmission(MMA8452_ADDRESS);
+  Wire.write(addressToWrite);
+  Wire.write(dataToWrite);
+  Wire.endTransmission(); //Stop transmitting
+}
+
+static const byte ASCII[][5] =
+{
+ {0x00, 0x00, 0x00, 0x00, 0x00} // 20  
+,{0x00, 0x00, 0x5f, 0x00, 0x00} // 21 !
+,{0x00, 0x07, 0x00, 0x07, 0x00} // 22 "
+,{0x14, 0x7f, 0x14, 0x7f, 0x14} // 23 #
+,{0x24, 0x2a, 0x7f, 0x2a, 0x12} // 24 $
+,{0x23, 0x13, 0x08, 0x64, 0x62} // 25 %
+,{0x36, 0x49, 0x55, 0x22, 0x50} // 26 &
+,{0x00, 0x05, 0x03, 0x00, 0x00} // 27 '
+,{0x00, 0x1c, 0x22, 0x41, 0x00} // 28 (
+,{0x00, 0x41, 0x22, 0x1c, 0x00} // 29 )
+,{0x14, 0x08, 0x3e, 0x08, 0x14} // 2a *
+,{0x08, 0x08, 0x3e, 0x08, 0x08} // 2b +
+,{0x00, 0x50, 0x30, 0x00, 0x00} // 2c ,
+,{0x08, 0x08, 0x08, 0x08, 0x08} // 2d -
+,{0x00, 0x60, 0x60, 0x00, 0x00} // 2e .
+,{0x20, 0x10, 0x08, 0x04, 0x02} // 2f /
+,{0x3e, 0x51, 0x49, 0x45, 0x3e} // 30 0
+,{0x00, 0x42, 0x7f, 0x40, 0x00} // 31 1
+,{0x42, 0x61, 0x51, 0x49, 0x46} // 32 2
+,{0x21, 0x41, 0x45, 0x4b, 0x31} // 33 3
+,{0x18, 0x14, 0x12, 0x7f, 0x10} // 34 4
+,{0x27, 0x45, 0x45, 0x45, 0x39} // 35 5
+,{0x3c, 0x4a, 0x49, 0x49, 0x30} // 36 6
+,{0x01, 0x71, 0x09, 0x05, 0x03} // 37 7
+,{0x36, 0x49, 0x49, 0x49, 0x36} // 38 8
+,{0x06, 0x49, 0x49, 0x29, 0x1e} // 39 9
+,{0x00, 0x36, 0x36, 0x00, 0x00} // 3a :
+,{0x00, 0x56, 0x36, 0x00, 0x00} // 3b ;
+,{0x08, 0x14, 0x22, 0x41, 0x00} // 3c <
+,{0x14, 0x14, 0x14, 0x14, 0x14} // 3d =
+,{0x00, 0x41, 0x22, 0x14, 0x08} // 3e >
+,{0x02, 0x01, 0x51, 0x09, 0x06} // 3f ?
+,{0x32, 0x49, 0x79, 0x41, 0x3e} // 40 @
+,{0x7e, 0x11, 0x11, 0x11, 0x7e} // 41 A
+,{0x7f, 0x49, 0x49, 0x49, 0x36} // 42 B
+,{0x3e, 0x41, 0x41, 0x41, 0x22} // 43 C
+,{0x7f, 0x41, 0x41, 0x22, 0x1c} // 44 D
+,{0x7f, 0x49, 0x49, 0x49, 0x41} // 45 E
+,{0x7f, 0x09, 0x09, 0x09, 0x01} // 46 F
+,{0x3e, 0x41, 0x49, 0x49, 0x7a} // 47 G
+,{0x7f, 0x08, 0x08, 0x08, 0x7f} // 48 H
+,{0x00, 0x41, 0x7f, 0x41, 0x00} // 49 I
+,{0x20, 0x40, 0x41, 0x3f, 0x01} // 4a J
+,{0x7f, 0x08, 0x14, 0x22, 0x41} // 4b K
+,{0x7f, 0x40, 0x40, 0x40, 0x40} // 4c L
+,{0x7f, 0x02, 0x0c, 0x02, 0x7f} // 4d M
+,{0x7f, 0x04, 0x08, 0x10, 0x7f} // 4e N
+,{0x3e, 0x41, 0x41, 0x41, 0x3e} // 4f O
+,{0x7f, 0x09, 0x09, 0x09, 0x06} // 50 P
+,{0x3e, 0x41, 0x51, 0x21, 0x5e} // 51 Q
+,{0x7f, 0x09, 0x19, 0x29, 0x46} // 52 R
+,{0x46, 0x49, 0x49, 0x49, 0x31} // 53 S
+,{0x01, 0x01, 0x7f, 0x01, 0x01} // 54 T
+,{0x3f, 0x40, 0x40, 0x40, 0x3f} // 55 U
+,{0x1f, 0x20, 0x40, 0x20, 0x1f} // 56 V
+,{0x3f, 0x40, 0x38, 0x40, 0x3f} // 57 W
+,{0x63, 0x14, 0x08, 0x14, 0x63} // 58 X
+,{0x07, 0x08, 0x70, 0x08, 0x07} // 59 Y
+,{0x61, 0x51, 0x49, 0x45, 0x43} // 5a Z
+,{0x00, 0x7f, 0x41, 0x41, 0x00} // 5b [
+,{0x02, 0x04, 0x08, 0x10, 0x20} // 5c ¥
+,{0x00, 0x41, 0x41, 0x7f, 0x00} // 5d ]
+,{0x04, 0x02, 0x01, 0x02, 0x04} // 5e ^
+,{0x40, 0x40, 0x40, 0x40, 0x40} // 5f _
+,{0x00, 0x01, 0x02, 0x04, 0x00} // 60 `
+,{0x20, 0x54, 0x54, 0x54, 0x78} // 61 a
+,{0x7f, 0x48, 0x44, 0x44, 0x38} // 62 b
+,{0x38, 0x44, 0x44, 0x44, 0x20} // 63 c
+,{0x38, 0x44, 0x44, 0x48, 0x7f} // 64 d
+,{0x38, 0x54, 0x54, 0x54, 0x18} // 65 e
+,{0x08, 0x7e, 0x09, 0x01, 0x02} // 66 f
+,{0x0c, 0x52, 0x52, 0x52, 0x3e} // 67 g
+,{0x7f, 0x08, 0x04, 0x04, 0x78} // 68 h
+,{0x00, 0x44, 0x7d, 0x40, 0x00} // 69 i
+,{0x20, 0x40, 0x44, 0x3d, 0x00} // 6a j 
+,{0x7f, 0x10, 0x28, 0x44, 0x00} // 6b k
+,{0x00, 0x41, 0x7f, 0x40, 0x00} // 6c l
+,{0x7c, 0x04, 0x18, 0x04, 0x78} // 6d m
+,{0x7c, 0x08, 0x04, 0x04, 0x78} // 6e n
+,{0x38, 0x44, 0x44, 0x44, 0x38} // 6f o
+,{0x7c, 0x14, 0x14, 0x14, 0x08} // 70 p
+,{0x08, 0x14, 0x14, 0x18, 0x7c} // 71 q
+,{0x7c, 0x08, 0x04, 0x04, 0x08} // 72 r
+,{0x48, 0x54, 0x54, 0x54, 0x20} // 73 s
+,{0x04, 0x3f, 0x44, 0x40, 0x20} // 74 t
+,{0x3c, 0x40, 0x40, 0x20, 0x7c} // 75 u
+,{0x1c, 0x20, 0x40, 0x20, 0x1c} // 76 v
+,{0x3c, 0x40, 0x30, 0x40, 0x3c} // 77 w
+,{0x44, 0x28, 0x10, 0x28, 0x44} // 78 x
+,{0x0c, 0x50, 0x50, 0x50, 0x3c} // 79 y
+,{0x44, 0x64, 0x54, 0x4c, 0x44} // 7a z
+,{0x00, 0x08, 0x36, 0x41, 0x00} // 7b {
+,{0x00, 0x00, 0x7f, 0x00, 0x00} // 7c |
+,{0x00, 0x41, 0x36, 0x08, 0x00} // 7d }
+,{0x10, 0x08, 0x08, 0x10, 0x08} // 7e ←
+,{0x78, 0x46, 0x41, 0x46, 0x78} // 7f →
+};
+
+void LcdCharacter(char character)
+{
+  LcdWrite(LCD_D, 0x00);
+  for (int index = 0; index < 5; index++)
+  {
+    LcdWrite(LCD_D, ASCII[character - 0x20][index]);
+  }
+  LcdWrite(LCD_D, 0x00);
+}
+
+void LcdClear(void)
+{
+  for (int index = 0; index < LCD_X * LCD_Y / 8; index++)
+  {
+    LcdWrite(LCD_D, 0x00);
+  }
+}
+
+void LcdInitialise(void)
+{
+  pinMode(PIN_SCE, OUTPUT);
+  pinMode(PIN_RESET, OUTPUT);
+  pinMode(PIN_DC, OUTPUT);
+  pinMode(PIN_SDIN, OUTPUT);
+  pinMode(PIN_SCLK, OUTPUT);
+  digitalWrite(PIN_RESET, LOW);
+  digitalWrite(PIN_RESET, HIGH);
+  LcdWrite(LCD_C, 0x21 );  // LCD Extended Commands.
+  LcdWrite(LCD_C, 0xB1 );  // Set LCD Vop (Contrast). 
+  LcdWrite(LCD_C, 0x04 );  // Set Temp coefficent. //0x04
+  LcdWrite(LCD_C, 0x14 );  // LCD bias mode 1:48. //0x13
+  LcdWrite(LCD_C, 0x0C );  // LCD in normal mode.
+  LcdWrite(LCD_C, 0x20 );
+  LcdWrite(LCD_C, 0x0C );
+}
+
+void LcdString(char *characters)
+{
+  while (*characters)
+  {
+    LcdCharacter(*characters++);
+  }
+}
+void setContrast(byte contrast)
+{  
+  LcdWrite(LCD_COMMAND, 0x21); //Tell LCD that extended commands follow
+  LcdWrite(LCD_COMMAND, 0x80 | contrast); //Set LCD Vop (Contrast): Try 0xB1(good @ 3.3V) or 0xBF if your display is too dark
+  LcdWrite(LCD_COMMAND, 0x20); //Set display mode
+}
+
+void LcdWrite(byte dc, byte data)
+{
+  digitalWrite(PIN_DC, dc);
+  digitalWrite(PIN_SCE, LOW);
+  shiftOut(PIN_SDIN, PIN_SCLK, MSBFIRST, data);
+  digitalWrite(PIN_SCE, HIGH);
+}
+void gotoXY(int x, int y)
+{
+  LcdWrite(0, 0x80 | x);  // Column.
+  LcdWrite(0, 0x40 | y);  // Row.  ?
+}
